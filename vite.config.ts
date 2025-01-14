1279f53c4a7b6d04d5ccc6692519061b524e445d
Mateusz Kifner <29428687+mateusz-kifner@users.noreply.github.com>
2025-01-14 23:55:17 +0100

    feat: added types defs via vite-plugin-dts

--- vite.config.ts
+++ vite.config.ts
@@ -2,11 +2,12 @@ import { resolve } from 'path'
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tsconfigPaths from "vite-tsconfig-paths"
+import dts from "vite-plugin-dts"
 
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tsconfigPaths()],
+  plugins: [react(), tsconfigPaths(), dts({ rollupTypes: true, tsconfigPath: './tsconfig.app.json' })],
   build: {
     lib: {
       entry: resolve(__dirname, 'lib/main.tsx'),
